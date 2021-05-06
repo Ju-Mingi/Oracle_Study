dc45fe4995d9d4ceaba2153493b74d8d387465be
Ju-Mingi <jmg961218@gmail.com>
2021-05-06 17:23:32 +0900

    first commit

---
+++ ora_user.sql
@@ -0,0 +1,29 @@
+CREATE TABLE ex2_1(
+COLUMN1 CHAR(10),
+COLUMN1 VARCHAR2(10),
+COLUMN1 NVARCHAR2(10),
+COLUMN1 NUMBER
+);
+
+INSERT INTO ex2_1 (column1, column2) VALUES ('abc','abc');
+
+SELECT column1, LENGTH(column1) as len1,
+column2, LENGTH(column2) as len2
+FROM ex2_1;
+
+CREATE TABLE ex2_2(
+COLUMN1 VARCHAR2(3),
+COLUMN2 VARCHAR2(3 byte),
+COLUMN3 VARCHAR2(3 char)
+);
+
+INSERT INTO ex2_2 VALUES('abc', 'abc', 'abc');
+
+SELECT column1, LENGTH(column1) AS len1,
+column2, LENGTH(column2) AS len2,
+column3, LENGTH(column3) AS len3
+FROM ex2_2;
+
+INSERT INTO ex2_2 (column3) VALUES ('ȫ�浿');
+
+SELECT column3, LENGTH(column3) AS len3, LENGTHB(column3) AS bytelen FROM ex2_2;
